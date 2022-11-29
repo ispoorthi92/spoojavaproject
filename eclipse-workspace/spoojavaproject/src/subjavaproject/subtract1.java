4b8fa7ee3768f2d266e18c55523ba15242f69b28
ispoorthi92 <ispoorthi92@gmail.com>
2022-11-29 17:02:05 +0530

    "commiting"

---
+++ eclipse-workspace/spoojavaproject/src/subjavaproject/subtract1.java
@@ -0,0 +1,18 @@
+package subjavaproject;
+
+public class subtract1 {
+ int i=13;
+     
+       int sub (int a,int b) {
+    	return a-b;
+    }
+	public static void main(String[] args) {
+		
+		subtract1 obj =new subtract1();
+		System.out.println(obj.i);
+		System.out.println(obj.sub(8, 4));
+		
+		
+	}
+
+}
